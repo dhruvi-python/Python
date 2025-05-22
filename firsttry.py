bb6a865c190cf356b7988a6cbb7604082617fa53
Parth6022 <dhruvipatel4482@gmail.com>
2025-05-22 14:13:18 +0530

    Python

---
+++ firsttry.py
@@ -0,0 +1,196 @@
+#include <studio.h>
+print("hello world")
+
+#print("hello world")
+
+#Arithmetic operators
+
+a = 10
+b = 5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+print(a // b)
+print(a % b)
+print(a , b)
+print(a)
+print(a > b)
+print(a < b)
+print(a == b)
+print(a!= b)
+print(a >=b)
+print(a <= b)
+print(a & b)
+print(a ^ b)
+print(a << b)
+print(a >> b)
+print(type(a))
+print('hello world')
+print("hello world")
+print("'hello world'")
+
+c=50/2
+d=20
+
+print(c)
+print(bin(58))
+print(oct(123))
+print(hex(50))
+print(chr(65))
+print(ord('A'))\
+
+print(a!=b and a<b)
+print(a!=b or a>b)
+name = input("enter your name:")
+print("hello," + name)
+print(type(name))
+
+num =(input("enter a number:"))
+print(num)
+print(type(num))
+
+
+num =int((input("enter a number:")))
+print(num)
+print(type(num))
+num=num+2
+print(num)
+
+num =bool((input("enter a number:")))
+print(num)
+print(type(num))
+
+var_1=2
+print(var_1)
+
+a=20
+A=30
+print(a)
+print(A)
+
+#1 int
+age=22
+print(type(age))
+
+#2 float
+weight=90.50
+print(type(weight))
+
+#3 string
+name = 'dhruvi'
+print(type(name))
+
+#4 boolean
+is_girl=True
+print(type(is_girl))
+
+#5 list
+list1=[10,20,30]
+print(type(list1))
+
+#6 dictionary
+dict1={'name':'dhruvi', 'age':22, 'city':'surat'}
+print(type(dict1))
+print(dict1['city'])
+print(dict1['name'])
+print(dict1['age'])
+
+#7 tuple
+tup=(20,40,60)
+print(type(tup))
+
+#8 set
+set1={1,2,3,4,5,6}
+print(type(set1))
+
+#9 none
+house_no=None
+print(type(house_no))
+
+#10 complex
+com=1+3j
+print(type(com))
+
+#11 range
+num=range(3,10)
+print(type(num))
+print(num)
+
+#12 bytes & bytearray
+byte = bytes([30,60,90])
+print(type(byte))
+print(byte)
+
+num1=input("enter first number")
+num2=input("enter second number")
+print(num1+num2)
+
+num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+print(num1+num2)
+
+num1=30
+num2=30.5
+print(num1+num2)
+print(num1*num2)
+
+a = 12
+f=float(a)
+print(f)
+
+b = hex(a)
+print(b)
+
+c = oct(a)
+print(c)
+
+d = bin(a)
+print(d)
+
+char = 'A'
+e = ord(char)
+print(e)
+e = 75
+char = chr(e)
+print(char)
+
+dhruvi = "cool girl"
+print("dhruvi")
+
+#escape character
+
+#1 \: backslash
+print('i\'ll call you later')
+#print('i'll call you later')
+#print("i"'ll call you later')
+
+#2\n: newline
+a = "hello,\ndhruvi!"
+print(a)
+
+#3\t: tab
+a = "hello,\tdhruvi!"
+print(a)
+
+#4\r: carriage return
+d = "no benetits of study\rhuge benefits"
+print(d)
+
+#if/if...else/if...elif...else statemate
+#if statemate
+age = int(input("enter your age"))
+if age > 18:
+    print("you can apply for license")
+    print("speed thrills but kills")
+
+#if...else statemate    
+if age > 50:
+    print(" you can apply")
+    
+else:
+    print("not visible")   
+
---
+++ prc1.py
@@ -0,0 +1,20 @@
+#2 How to create variable in python?
+a = 'dhruvi'
+print(a)
+print(type(a))
+
+#3 How to take user input?
+name = input('enter your name')
+birthdate = input('enter your birth date')
+print()
+
+#4 How to check the type of variable dynamically.?
+d = 50
+print(type(50))
+b = 'vanshika'
+print(type(b))
+
+#5 W.A.P to find greater and less than number using If_else?
+x = 20
+y = 10
+
