474d8bd3392982a33d3cf20019c23c13fdff8a9e
Parth6022 <dhruvipatel4482@gmail.com>
2025-05-24 15:38:57 +0530

    task2.py

---
+++ task2.py
@@ -0,0 +1,30 @@
+#task 1
+num = int(input("enter a number"))
+
+if ("num / 1 && num / num"):
+            print("num is prime")
+else:
+          print("num is not prime")
+
+#task 2 
+# num1 = int(input("enter the first number"))
+# num2 = int(input("enter the second number"))
+# if num1 < num2:
+#     print("the big number is num2")
+# elif num1 > num2:
+#     print("the big number is num1")
+
+#task 3
+# num = int(input("enter a n number"))
+# if ("num = 1 and num = num"):
+#     # for i in range(2, num):
+#      if num % 1 == 0:
+#         if num == 3:
+#             print("num is prime")
+#         else:
+#           print("num is not prime")
+
+
+
+
+
