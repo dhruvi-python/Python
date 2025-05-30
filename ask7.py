63ab7070a4739ff0e96b471f59745db9fac961cb
Parth6022 <dhruvipatel4482@gmail.com>
2025-05-30 13:48:49 +0530

    ask7.py

---
+++ ask7.py
@@ -0,0 +1,10 @@
+num1 = (int(input("enter your first number")))
+# num2 = (int(input("enter your second number")))
+if num1 > 0:
+    print("num is big")
+elif num1 < 0:
+    print("num is small")
+else:
+    print("num is eqal")
+
+        
