e492f77273c430297cea73783083833537c60fd3
Parth6022 <dhruvipatel4482@gmail.com>
2025-06-02 16:34:28 +0530

    task10.py

---
+++ task10.py
@@ -0,0 +1,8 @@
+age = int(input("enter your age"))
+weight = float(input("enter your weight"))
+if age > 18:
+ print("you can donate blood")
+if (weight < 50):
+  print("you can not donted blood")
+else:
+ print("you can donate but check your blood percentage")
