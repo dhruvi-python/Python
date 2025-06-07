38a412378c1054c06d0d363c893aa6a2c15222be
Parth6022 <dhruvipatel4482@gmail.com>
2025-06-07 14:56:39 +0530

    task13.py

---
+++ task13.py
@@ -0,0 +1,13 @@
+name = "dhruvi"
+print("hello" + name)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[-4:-2])
+    
+# msg = "hi"
+# times = 4.9
+# print(msg*int(times))
