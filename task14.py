7b8eb2987e849f39786af13c95c89898153adeb8
Parth6022 <dhruvipatel4482@gmail.com>
2025-06-08 22:10:08 +0530

    task14.py

---
+++ task14.py
@@ -0,0 +1,14 @@
+
+
+quantity = int(input("enter the iquantity"))
+price=50
+print("coffee")  
+if (quantity >= 3):
+     print("You have 20% discount")
+else:
+    print("you have no discount")
+
+
+
+    
+
