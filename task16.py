22b7611b7863abe747e76890194bd3b5f76cb57c
Parth6022 <dhruvipatel4482@gmail.com>
2025-06-10 14:54:21 +0530

    task16.py

---
+++ task16.py
@@ -0,0 +1,15 @@
+# WAP to print each fruit in list using simple for loop. List1 (apple,banana,mango)
+fruits = ["banana", "kv", "mango"]
+for x in fruits:
+    print(x) 
+    
+# WAP to find the length of string using simple for loop List1 (apple,banana,mango)
+list1 = ["apple","banana","mango"]
+for list1 in list1:
+ print(len(list1))
+
+
+
+
+
+
