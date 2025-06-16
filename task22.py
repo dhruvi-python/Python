48b5b3606854bff20f74b5f1b9aa9b8d3fc9d551
Parth6022 <dhruvipatel4482@gmail.com>
2025-06-16 14:16:22 +0530

    task22.py

---
+++ task22.py
@@ -0,0 +1,24 @@
+x = lambda a: a + 10
+print(x(15))
+
+# arrays
+cars = ["Ford", "Volvo", "BMW"]
+
+x = cars[1]
+
+print(x)
+
+# table = 2
+
+for i in range(1,11):
+    # print(2,i*1,i*2)
+    print(f"2 * {i} = {i*2}")
+
+# for i in range(15):
+#     print(i*3)
+    
+
+
+
+
+
