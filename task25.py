d3800e6f8fbf6852d3b9d0a4497397dfe483bdbb
Parth6022 <dhruvipatel4482@gmail.com>
2025-06-19 14:49:39 +0530

    task25.py

---
+++ task25.py
@@ -0,0 +1,65 @@
+# write a program to print a string "Hello Python".
+print("hello python")
+
+# write a program to store one value in variable and print that variable.
+a = "dhruvi"
+print(a)
+
+num = 10
+print(num)
+
+# write a program to take value from user input and print that value.
+number = input("enter the number you want")
+print(number)
+
+# write a program to do a sum of two  value.
+a = 50
+b = 30
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = num1 + num2
+sum = num1 - num2
+sum = num1 * num2
+sum = num1 / num2
+
+print(sum)
+
+# write a program to perform division using variables
+num3 = int(input("enter third number"))
+num4 = int(input("enter fourth number"))
+sum1 = num3 % num4
+sum1 = num3/num4
+print(sum1)
+
+# write a program to print multiple value of variables
+name = "dhruvi"
+hobbies = "dancing"
+age = 22
+num = 10.10
+print(name)
+print(hobbies)
+print(age)
+print(num)
+
+# write a program to find a type of variable
+hobbies = "dancing"
+age = 22
+num = 10.10
+print(type(hobbies))
+print(type(age))
+print(type(num))
+
+# write a program to take two integer value from the user and check both the values are same.
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1 == num2:
+    print("both number are same")
+else:
+    print("not same number")
